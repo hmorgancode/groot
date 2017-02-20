ee1db6e9ece6c067fd766c545aa504f2aa377714
Henry Morgan <hemorgan@wayfair.com>
2017-02-19 21:28:04 -0500

    add more styling, descriptions, tags, and initial meter that is definitely getting replaced

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PlantList from './components/PlantList';
+import { plants } from './test/testData';
 // import logo from './logo.svg';
 import './App.css';
 // import '../node_modules/font-awesome/css/font-awesome.min.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-        <PlantList />
+        <PlantList plants={plants} />
     );
   }
 }
---
+++ src/components/Plant.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Truncate from 'react-truncate';
+
+class Plant extends React.Component {
+
+
+  render() {
+    const plant = this.props.plant;
+    // Move this to a Tag component and pass tags and guideMode
+    // let guideModeTags = '', standardModeTags = '';
+    // if (this.props.guideMode) {
+    //   guideModeTags = plant.tags.map((tag) => <span key={tag} className="tag is-success">{tag}</span>);
+    // } else {
+    //   standardModeTags = plant.tags.map((tag) => <span key={tag} className="tag is-success">{tag}</span>);
+    // }
+    return (
+      <article key={plant.id} className="media box">
+        <div className="media-left">
+          <img className="image is-64x64" src={plant.thumbnail} alt={plant.name}></img>
+        </div>
+        <div className="media-content">
+          <strong>{plant.name}</strong>
+          {plant.tags.map((tag) => <span key={tag} className="tag is-success">{tag}</span>)}
+          <br />
+
+          <small>{`(${plant.altName})`}</small>
+          <br />
+
+          {/* Just cut off any instruction set that gets too long. */}
+          <div className="plant-description">
+            <Truncate lines={2} ellipsis={''}>
+              <p>{plant.instructions}</p>
+            </Truncate>
+          </div>
+
+          <nav className="level">
+            <meter className="meter is-info"
+                      value={plant.soilMoisture || plant.waterLevel} max="1">
+              {plant.waterLevel ? 'Water Level' : 'Soil Moisture'}
+            </meter>
+          </nav>
+        </div>
+      </article>
+    );
+  }
+}
+
+export default Plant;
