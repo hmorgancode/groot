cf9b7891b8acad2e7826c4aa39643b1063559447
Henry Morgan <hmorgan471@gmail.com>
2017-04-29 17:18:29 -0400

    make plant view expandable

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -3,7 +3,14 @@ import WaterMeter from './WaterMeter';
 import Truncate from 'react-truncate';
 
 class Plant extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {expanded: false};
+  }
 
+  expandBox() {
+    this.setState({expanded: !this.state.expanded});
+  }
 
   render() {
     const plant = this.props.plant;
@@ -15,26 +22,32 @@ class Plant extends React.Component {
     //   standardModeTags = plant.tags.map((tag) => <span key={tag} className="tag is-success">{tag}</span>);
     // }
     return (
-      <article key={plant.id} className="media box">
+      <article key={plant.id} className="media box {this.state.expanded && 'expanded'}" onClick={this.expandBox.bind(this)}>
         <div className="media-left">
           <img className="image is-64x64" src={plant.thumbnail} alt={plant.name}></img>
         </div>
         <div className="media-content">
           <strong>{plant.name}</strong>
-          {plant.tags.map((tag) => <span key={tag} className="tag is-success">{tag}</span>)}
           <br />
 
-          <small>{`(${plant.altName})`}</small>
-          <br />
+          <div>
+            <small>{`(${plant.altName})`}</small>
+            <br />
+          </div>
+          <WaterMeter plant={plant} />
+
+          { this.state.expanded && plant.tags.length > 0 && plant.tags.map((tag) => <span key={tag} className="tag is-success">{tag}</span>) }
+          { this.state.expanded && plant.tags.length > 0 && <br />}
 
           {/* Just cut off any instruction set that gets too long. */}
-          <div className="plant-description">
-            <Truncate lines={2} ellipsis={''}>
-              <p>{plant.instructions}</p>
-            </Truncate>
-          </div>
+          { this.state.expanded &&
+            <div className="plant-description">
+              <Truncate lines={4} ellipsis={''}>
+                <p>{plant.instructions}</p>
+              </Truncate>
+            </div>
+          }
 
-          <WaterMeter plant={plant} />
 
         </div>
       </article>
--- src/components/WaterMeter.js
+++ src/components/WaterMeter.js
@@ -7,12 +7,12 @@ class WaterMeter extends React.Component {
   render() {
     const plant = this.props.plant;
     return (
-      <nav className="level">
+      <nav className="">
             <meter className="meter is-info"
                       value={plant.soilMoisture || plant.waterLevel} max="1">
               {plant.waterLevel ? 'Water Level' : 'Soil Moisture'}
             </meter>
-        </nav>
+      </nav>
     );
   }
 }
