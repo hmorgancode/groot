458f44eb32ea88f767802c6fcd5fdb04840e3828
Henry Morgan <hmorgan471@gmail.com>
2017-09-26 12:36:42 -0400

    remove local state from nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,79 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { goToPlants, goToBoards, openModal } from '../redux/actionTypes';
-// import AddPlantForm from './AddPlantForm';
-// import ReactModal from 'react-modal';
-// ReactModal.setAppElement('#i-am-root');
 
 class NavBar extends React.Component {
 
-  state = {
-    activeTab: 'Plants'
-  };
-
-  /**
-   * Changes the currently active tab to the one selected.
-   * @param  {string} tabName The name of the tab. Right now these
-   *                          are just self-documented in render().
-   */
-  changeTab = (tabName) => {
-    if (this.state.activeTab !== tabName) {
-      this.setState({ activeTab: tabName });
-    }
-  };
-
   isActiveTab = (tabName) => {
-    return tabName === this.state.activeTab ? 'is-active' : '';
-  };
-
-  // handleOpenModal() {
-  //   this.setState({ showModal: true });
-  // }
-  // handleCloseModal() {
-  //   this.setState({ showModal: false });
-  // }
+    return tabName === this.props.activePage ? 'is-active' : '';
+  }
 
   render() {
     return (
       <div id="nav-bar" className="tabs is-toggle is-fullwidth">
         <ul>
-          <li onClick={() => { this.changeTab('Plants'); this.props.handleGoToPlants(); } } className={this.isActiveTab('Plants')}>
+          <li onClick={() => { this.props.handleGoToPlants(); } } className={this.isActiveTab('PLANTS')}>
             <a>
               <span className="icon"><i className="fa fa-pagelines"></i></span>
               <span>Plants</span>
             </a>
           </li>
-          <li onClick={() => { this.changeTab('Sensors'); this.props.handleGoToBoards(); } } className={this.isActiveTab('Sensors')}>
+          <li onClick={() => { this.props.handleGoToBoards(); } } className={this.isActiveTab('BOARDS')}>
             <a>
               <span className="icon"><i className="fa fa-bolt"></i></span>
               <span>Boards</span>
             </a>
           </li>
-          <li onClick={ () => this.props.handleOpenModal('ADD_PLANT') } className={this.isActiveTab('Add/Remove')}>
+          <li onClick={ () => this.props.handleOpenModal('ADD_PLANT') }>
             <a>
               <span className="icon"><i className="fa fa-cogs"></i></span>
               <span>Add/Remove</span>
             </a>
           </li>
         </ul>
-
-        {/*<ReactModal
-          isOpen={ this.state.showModal }
-          onAfterOpen={ this.afterOpenModal }
-          contentLabel="Add Plant Window"
-          onRequestClose={ this.handleCloseModal }
-          className="Modal"
-          overlayClassName="Overlay"
-        >
-          <AddPlantForm handleCloseModal={this.handleCloseModal} />
-        </ReactModal>*/}
       </div>
     );
   }
 }
 
 const NavBarWithState = connect(
-  null, // nav bar doesn't use store data right now
+  (state) => ({ activePage: state.app.activePage }),
   (dispatch) => ({
     handleOpenModal(modalType) {
       dispatch(openModal(modalType));
